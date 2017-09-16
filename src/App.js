5a7f1c4863da68cc2cb84c1368943924bba5a4fc
정지원 <4dakto@gmail.com>
2017-09-17 01:27:03 +0900

    intercept route chages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Prompt } from 'react-router-dom';
 import './App.css';
-
-const loggedin = true
 
 const Links = () =>
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/old/123">Old</Link>
-    <Link to="/new/456">New</Link>
-    <Link to="/protected">Protected</Link>
+    <Link to="/form">Form</Link>
   </nav>
 
+const Home = () => (<h1>Home</h1>)
+class Form extends Component {
+  state = {
+    dirty: false
+  }
+  setDirty = () => this.setState({ dirty: true })
+  
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <input type="text" onInput={this.setDirty}/>
+        <Prompt
+          when={this.state.dirty}
+          message="Data will be lost!"
+        />
+      </div>
+    )
+  }
+}
 const App = (props) =>(
   <Router>
     <div>
       <Links />
       <Switch>
-        <Route exact path="/" render={() => (<h1>Home</h1>)} />
+        <Route exact path="/" render={Home} />
         <Route
-          path="/new/:str"
-          render={({ match }) => <h1>New: {match.params.str}</h1>} />
-        <Route
-          path="/old/:str"
-          render={({ match }) => (
-            <Redirect to={`/new/${match.params.str}`} />
-          )} />
-        <Route
-          path="/protected"
-          render={({ match }) => (
-            loggedin
-            ? <h1>Welcome to the protected page</h1>
-            : <Redirect to="/new/Login" />
-          )} />
+          path="/form"
+          component={Form}/>
       </Switch>
     </div>
   </Router>
