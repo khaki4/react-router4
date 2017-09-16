a57fa7836b720a01adf818d6968dd6dd534ebca6
정지원 <4dakto@gmail.com>
2017-09-16 15:32:32 +0900

    Use the React Router v4 Link Component for Navigation Between Routes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+## react router
+[https://reacttraining.com/react-router/web/guides/philosophy]
+
 ## install react router 4
 `yarn add -D react-router-dom@next`
--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import {
   Link
 } from 'react-router-dom';
 
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link replace to={{pathname: '/contact'}}>Contact</Link>
+  </nav>
+)
+
 const Home = (props) => {
   console.log(props)
   return <h1>Home</h1>
@@ -14,12 +22,10 @@ const Home = (props) => {
 const App = (props) => (
   <Router>
     <div>
+      <Links />
       <Route exact path="/" component={Home} />
-      {/*<Route path="/about" render={() => <h1>About</h1>} />*/}
-      <Route
-        path="/about"
-        children={({match}) => match && <h1>About</h1>}
-      />
+      <Route path="/about" render={() => <h1>About</h1>} />
+      <Route path="/contact" render={() => <h1>Contact</h1>} />
     </div>
   </Router>
 );
