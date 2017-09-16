b9e04dc5aa4193204d0f51710fc6b3b1e08d863e
정지원 <4dakto@gmail.com>
2017-09-16 21:15:27 +0900

    Render Nested Routes with React Router v4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 
-const Links = () =>
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
-    <Link to="/test/:token">test</Link>
-  </nav>
+const Home = () => (<h1>Home</h1>)
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
+    <Link to="/menu/food">Food</Link>
+    <Link to="/menu/drink">Drinks</Link>
+    <Link to="/menu/sides">Sides</Link>
+    <Route
+      path="/menu/:section"
+      render={({ match }) => (
+        <h2>{match.params.section}</h2>
+      )}
+    />
+  </div>
+)
 
-class App extends Component {
-  render () {
-    return (
-      <Router>
-        <div>
-          <Links />
-          <Switch>
-            <Route exact path="/" render={() => <h1>Home</h1>} />
-            <Route path="/about" render={() => <h1>About</h1>} />
-            <Route path="/contact" render={() => <h1>Contact</h1>} />
-            <Route path="/test/:token"
-                   render={({ match }) => <h1>Test: {match.params.token}</h1>} />
-            <Route path="/:itemid"
-                   render={({ match }) => <h1>Item: {match.params.itemid}</h1>} />
-          </Switch>
-        </div>
-      </Router>
-    )
-  }
-}
+const App = (props) =>(
+  <Router>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Memu</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/menu" component={Menu} />
+    </div>
+  </Router>
+)
 
 export default App;
